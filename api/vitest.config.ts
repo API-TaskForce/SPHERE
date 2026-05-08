7b903c2f74b9791d36d56d4c2e3fe3c4ccd2faaa
rafapulido <rafael.pulido@sagasoluciones.com>
2026-05-08 12:52:53 +0200

    feat: Complete implementation of the test suite

--- api/vitest.config.ts
+++ api/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
       include: ['**/*.test.ts'], // Solo incluye los tests del directorio API
+      fileParallelism: false,
+      setupFiles: ['./tests/utils/setup.ts'],
       globals: true, // Habilita la API global de Vitest
       environment: 'node', // Usa el entorno Node.js
       typecheck: {
